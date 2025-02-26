94a513babd6ca01974ad3cacb07a20eff1ae55d6
houdinimain <houdgondal@icloud.com>
2025-02-26 18:56:29 +0000

    Add files via upload

---
+++ tuples1 - Copy.py
@@ -0,0 +1,44 @@
+fruit = ("apples","bannana","pear","orange","pineapple")
+
+print(type(fruit)) #its a read only datatype which cannot be changed  
+
+print(len(fruit))
+
+print(fruit[0])
+
+
+print(fruit[2])
+print(fruit[0:3])
+
+
+
+print(fruit[::-1])
+
+
+
+if "bannana" in fruit:
+    print("it exists")
+
+if "pear" not in fruit:
+    print("it does not exist")
+
+
+else:
+    print("it doesnt exist")
+
+
+
+
+tuple1 = (1,2,3,4,5,6,7,8,9)
+tuple2 = (9,8,7,6,5,4,3,2,1)
+
+
+
+
+print(tuple1 + tuple2 )
+
+
+
+
+
+
---
+++ tuples2 - Copy.py
@@ -0,0 +1,17 @@
+name,grade,school,age,city = ("houd",9,"burnham",13,"london")
+print(name)
+print(grade)
+print(school)
+print(age)
+print(city)
+fruit = ("apples","bannana","pear","orange","pineapple")
+
+for i in fruit:
+    print (i)
+
+
+
+
+
+
+
